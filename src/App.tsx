2f3f9e841c759e0e5d42e9641fd4274b99f09fde
park-chanhyung <chanhyung13@gmail.com>
2024-03-13 14:58:23 +0900

    파이어베이스 로그인 로딩

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+
+//라우터 설정 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+// 스타일 
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+function App() {
+  const[isLoading,setLoading] = useState(true);
+  const init = async() => {
+    // 파이어베이스 로딩 코드 
+    setLoading(false);
+  };
+  useEffect(()=>{
+    init();
+  },[]);
+
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen/> : <RouterProvider router={router}/>}
+    </>
+  );
+}
+
+export default App;
